0153542d9fee40cc7b9d18a042dfc366723367be
DanielAdek <maildaniel.me1@gmail.com>
2023-09-20 12:45:22 +0100

    complete setup for microservices

--- task-microservice/src/main.ts
+++ task-microservice/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {Logger} from "@nestjs/common";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const logger: Logger = new Logger();
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqps://nifgfcyd:XjzIKX8FXA5jWTd3imueuIKXwx_aDYeQ@moose.rmq.cloudamqp.com/nifgfcyd'],
+      queue: 'user_queue',
+      queueOptions: {
+        durable: false
+      },
+    },
+  });
+  await app.listen();
+  logger.log("TASK MICROSERVICE LISTENING!");
 }
-bootstrap();
+(async function (): Promise<void>{
+  await bootstrap();
+})();
